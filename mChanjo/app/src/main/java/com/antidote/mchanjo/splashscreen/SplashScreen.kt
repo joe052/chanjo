d8a571266db09ff42d862a645f292e4eef194fee
joe052 <joekaranjasenior52@gmail.com>
2023-07-08 15:32:38 +0300

    added splash image

---
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/splashscreen/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.antidote.mchanjo.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antidote.mchanjo.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
