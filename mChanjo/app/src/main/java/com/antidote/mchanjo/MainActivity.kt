c3ab97ef5fab773bc8b52d64113a270c3f3f744c
blackcode-creator <legacyitsolution@gmail.com>
2023-07-09 15:01:19 +0300

    fixed the splash screen and pending logic for splash screen

--- mChanjo/app/src/main/java/com/antidote/mchanjo/MainActivity.kt
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//The below line hides the actionbar to give the splash screen a clear view
+        supportActionBar?.hide()
     }
 }
---
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,34 @@
+package com.antidote.mchanjo.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.antidote.mchanjo.R
+import com.antidote.mchanjo.onboarding.screens.FirstScreen
+import com.antidote.mchanjo.onboarding.screens.SecondScreen
+import com.antidote.mchanjo.onboarding.screens.ThirdScreen
+
+class ViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        This will return the view of the fragments
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+            FirstScreen(),
+            SecondScreen(),
+            ThirdScreen()
+        )
+        val adapter = ViewPagerAdapter(fragmentList,requireActivity().supportFragmentManager, lifecycle)
+        view.findViewById<ViewPager2>(R.id.viewPager).adapter = adapter
+        return view
+    }
+
+}
