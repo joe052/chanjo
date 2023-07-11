5b4f5e4b1e240b80d06ea9ed647d6397df3c5308
Immanuel Kimani <immanuel4082@gmail.com>
2023-07-11 05:14:43 +0300

    message

---
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/ChooseClient.kt
@@ -0,0 +1,11 @@
+package com.antidote.mchanjo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseClient : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_client)
+    }
+}
