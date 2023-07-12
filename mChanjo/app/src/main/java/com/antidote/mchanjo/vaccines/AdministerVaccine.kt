f28cfeba25d730982ba8c098bfdf2af72321bc69
Immanuel Kimani <immanuel4082@gmail.com>
2023-07-12 16:37:03 +0300

    message

---
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/vaccines/AdministerVaccine.kt
@@ -0,0 +1,12 @@
+package com.antidote.mchanjo.vaccines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antidote.mchanjo.R
+
+class AdministerVaccine : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_vaccine)
+    }
+}
---
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/vaccines/Covid.kt
@@ -0,0 +1,12 @@
+package com.antidote.mchanjo.vaccines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antidote.mchanjo.R
+
+class Covid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_covid_19_vaccine)
+    }
+}
