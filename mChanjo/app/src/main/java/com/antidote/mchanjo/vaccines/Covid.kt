919f509a78d6fea0e560b8a0908f1984065d1868
Immanuel Kimani <immanuel4082@gmail.com>
2023-07-12 23:01:01 +0300

    message

--- mChanjo/app/src/main/java/com/antidote/mchanjo/vaccines/Covid.kt
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/vaccines/Covid.kt
@@ -15,6 +15,9 @@ import java.util.Locale
 class Covid : AppCompatActivity() {
 
     private lateinit var autoCompleteTextView: AutoCompleteTextView
+    private lateinit var autoCompleteTextView1: AutoCompleteTextView
+    private lateinit var autoCompleteTextView2: AutoCompleteTextView
+    private lateinit var autoCompleteTextView3: AutoCompleteTextView
     private val calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,16 +26,25 @@ class Covid : AppCompatActivity() {
 
         //select option text view
         autoCompleteTextView = findViewById(R.id.auto_complete_txt)
+        autoCompleteTextView1 = findViewById(R.id.auto_complete_txt1)
+        autoCompleteTextView2 = findViewById(R.id.auto_complete_txt2)
+        autoCompleteTextView3 = findViewById(R.id.auto_complete_txt3)
 
-        val options = arrayOf("AstraZeneca/Oxford vaccine",
-                "Johnson and Johnson",
-                "Moderna" ,
-                "Pfizer/BioNTech",
-                "Sinopharm",
-                "Sinovac",
-                "COVAXIN",
-                "Covovax" ,
-                "Nuvaxovid")
+        setupAutoCompleteTextView(autoCompleteTextView1)
+        setupAutoCompleteTextView(autoCompleteTextView2)
+        setupAutoCompleteTextView(autoCompleteTextView3)
+
+        val options = arrayOf(
+            "AstraZeneca/Oxford vaccine",
+            "Johnson and Johnson",
+            "Moderna",
+            "Pfizer/BioNTech",
+            "Sinopharm",
+            "Sinovac",
+            "COVAXIN",
+            "Covovax",
+            "Nuvaxovid"
+        )
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, options)
         autoCompleteTextView.setAdapter(adapter)
@@ -40,86 +52,30 @@ class Covid : AppCompatActivity() {
 
         autoCompleteTextView.setOnItemClickListener { parent, view, position, id ->
             val selectedOption = parent.getItemAtPosition(position) as String
-            Toast.makeText(applicationContext, "selected: $selectedOption", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "selected: $selectedOption", Toast.LENGTH_SHORT)
+                .show()
         }
+    }
 
-        //date picker text view
-        autoCompleteTextView = findViewById(R.id.auto_complete_txt1)
-
-        val datePickerListener1 = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
-            calendar.set(Calendar.YEAR, year)
-            calendar.set(Calendar.MONTH, monthOfYear)
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateDateField()
-        }
-
-        autoCompleteTextView.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                val datePickerDialog = DatePickerDialog(
-                    this,
-                    datePickerListener1,
-                    calendar.get(Calendar.YEAR),
-                    calendar.get(Calendar.MONTH),
-                    calendar.get(Calendar.DAY_OF_MONTH)
-                )
-                datePickerDialog.show()
-            }
-        }
-
-        // Set up the AutoCompleteTextView with date suggestions
-        val dateFormat1 = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-        val dateSuggestions1 = arrayOf(
-            dateFormat1.format(Date()),  // Today's date
-            dateFormat1.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time),  // Tomorrow's date
-            dateFormat1.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time)   // Next day's date
+    private fun setupAutoCompleteTextView(autoCompleteTextView: AutoCompleteTextView) {
+        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        val dateSuggestions = arrayOf(
+            dateFormat.format(Date()),  // Today's date
+            dateFormat.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time),  // Tomorrow's date
+            dateFormat.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time)   // Next day's date
         )
 
-        val adapter2 = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, dateSuggestions1)
-        autoCompleteTextView.setAdapter(adapter2)
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, dateSuggestions)
+        autoCompleteTextView.setAdapter(adapter)
 
-        autoCompleteTextView = findViewById(R.id.auto_complete_txt2)
-
-        val datePickerListener2 = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
-            calendar.set(Calendar.YEAR, year)
-            calendar.set(Calendar.MONTH, monthOfYear)
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateDateField()
-        }
-
-        autoCompleteTextView.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                val datePickerDialog = DatePickerDialog(
-                    this,
-                    datePickerListener2,
-                    calendar.get(Calendar.YEAR),
-                    calendar.get(Calendar.MONTH),
-                    calendar.get(Calendar.DAY_OF_MONTH)
-                )
-                datePickerDialog.show()
+        val datePickerListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, monthOfYear)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateField(autoCompleteTextView)
             }
-        }
-
-        // Set up the AutoCompleteTextView with date suggestions
-        val dateFormat2 = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-        val dateSuggestions2 = arrayOf(
-            dateFormat2.format(Date()),  // Today's date
-            dateFormat2.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time),  // Tomorrow's date
-            dateFormat2.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time)   // Next day's date
-        )
-
-        val adapter3 = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, dateSuggestions2)
-        autoCompleteTextView.setAdapter(adapter3)
-
-
-
-        autoCompleteTextView = findViewById(R.id.auto_complete_txt3)
-
-        val datePickerListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
-            calendar.set(Calendar.YEAR, year)
-            calendar.set(Calendar.MONTH, monthOfYear)
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateDateField()
-        }
 
         autoCompleteTextView.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
@@ -133,25 +89,10 @@ class Covid : AppCompatActivity() {
                 datePickerDialog.show()
             }
         }
+    }
 
-        // Set up the AutoCompleteTextView with date suggestions
-        val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-        val dateSuggestions = arrayOf(
-            dateFormat.format(Date()),  // Today's date
-            dateFormat.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time),  // Tomorrow's date
-            dateFormat.format(calendar.apply { add(Calendar.DAY_OF_MONTH, 1) }.time)   // Next day's date
-        )
-
-        val adapter1 = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, dateSuggestions)
-        autoCompleteTextView.setAdapter(adapter1)
-
-
-    }private fun updateDateField() {
+    private fun updateDateField(autoCompleteTextView: AutoCompleteTextView) {
         val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
         autoCompleteTextView.setText(dateFormat.format(calendar.time))
     }
-
-
-
-
 }
