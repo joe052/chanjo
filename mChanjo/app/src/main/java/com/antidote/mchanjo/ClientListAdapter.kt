db17889272f11c94bc0ef93bf037378e1256c7de
Immanuel Kimani <immanuel4082@gmail.com>
2023-07-13 04:59:39 +0300

    message

--- mChanjo/app/src/main/java/com/antidote/mchanjo/ClientListAdapter.kt
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/ClientListAdapter.kt
@@ -5,9 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.antidote.mchanjo.R
-import com.antidote.mchanjo.Client
-
 
 class ClientListAdapter(private val clientList: List<Client>) :
     RecyclerView.Adapter<ClientListAdapter.ClientViewHolder>() {
