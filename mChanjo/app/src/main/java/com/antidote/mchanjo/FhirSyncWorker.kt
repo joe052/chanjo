5c5014eb9f74b1a1a2825de2b629b7936b9dfbc3
joe052 <joekaranjasenior52@gmail.com>
2023-07-22 07:11:02 +0300

    message

--- mChanjo/app/src/main/java/com/antidote/mchanjo/FhirSyncWorker.kt
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/FhirSyncWorker.kt
@@ -5,7 +5,7 @@ import androidx.work.WorkerParameters
 import com.google.android.fhir.sync.AcceptLocalConflictResolver
 import com.google.android.fhir.sync.FhirSyncWorker
 
-class FhireSyncWorker(appContext: Context, workerParams: WorkerParameters) :
+class FhirSyncWorker(appContext: Context, workerParams: WorkerParameters) :
     FhirSyncWorker(appContext, workerParams)  {
     override fun getDownloadWorkManager() = DownloadWorkManagerImpl()
 
