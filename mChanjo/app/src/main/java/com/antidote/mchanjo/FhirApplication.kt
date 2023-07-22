36567b4d374f0a81fb30c1cc76e7b26a80800f8a
joe052 <joekaranjasenior52@gmail.com>
2023-07-22 04:47:42 +0300

    message

---
+++ mChanjo/app/src/main/java/com/antidote/mchanjo/FhirApplication.kt
@@ -0,0 +1,43 @@
+package com.antidote.mchanjo
+
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import com.google.android.fhir.BuildConfig
+import com.google.android.fhir.DatabaseErrorStrategy.RECREATE_AT_OPEN
+import com.google.android.fhir.FhirEngine
+import com.google.android.fhir.FhirEngineConfiguration
+import com.google.android.fhir.FhirEngineProvider
+import com.google.android.fhir.ServerConfiguration
+import com.google.android.fhir.sync.remote.HttpLogger
+
+class FhirApplication : Application() {
+    //  lazily instantiate an actual FHIR Engine instance
+    private val fhirEngine: FhirEngine by lazy { FhirEngineProvider.getInstance(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        FhirEngineProvider.init(
+            FhirEngineConfiguration(
+                enableEncryptionIfSupported = true,
+                RECREATE_AT_OPEN,
+                ServerConfiguration(
+//          baseUrl = "http://10.0.2.2:8080/fhir/",
+                    baseUrl = "https://7cdb-102-68-76-201.ngrok-free.app/fhir/",
+                    httpLogger =
+                    HttpLogger(
+                        HttpLogger.Configuration(
+                            if (BuildConfig.DEBUG) HttpLogger.Level.BODY else HttpLogger.Level.BASIC
+                        )
+                    ) { Log.d("App-HttpLog", it) },
+                ),
+            )
+        )
+    }
+
+    companion object {
+        // convenience method for the rest of the codelab
+        fun fhirEngine(context: Context) = (context.applicationContext as FhirApplication).fhirEngine
+    }
+}
